f52f2b89927b812f1bf34a558211ba493cfd4624
Rogerio Muniz <rogerio@quilhasoft.com.br>
2016-10-25 09:08:39 -0200

    Adicionado layout para boleto

--- autoloader.php
+++ autoloader.php
@@ -4,7 +4,7 @@
 *
 * LICENSE: The MIT License (MIT)
 *
-* Copyright (C) 2013 Ciatec.net
+* Copyright (C) 2013 Quilhasoft.net
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this
 * software and associated documentation files (the "Software"), to deal in the Software
